57f99cf99d570b5e82b8d0bb37c0ffec8ae67d24
Peter Kosztolanyi <peter.kosztolanyi@transferwise.com>
2019-07-04 16:02:53 +0100

    add long_description

--- setup.py
+++ setup.py
@@ -2,9 +2,14 @@
 
 from setuptools import setup
 
+with open('README.md') as f:
+      long_description = f.read()
+
 setup(name='pipelinewise-tap-snowflake',
       version='1.0.2',
       description='Singer.io tap for extracting data from Snowflake - PipelineWise compatible',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       author="TransferWise",
       url='https://github.com/transferwise/pipelinewise-tap-postgres',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
