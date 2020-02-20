8cfe3765d5c9d4f041b4a3f913df2a533fb65351
Samira-El <54845154+Samira-El@users.noreply.github.com>
2020-02-20 10:19:12 +0200

    bump minor

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
       long_description = f.read()
 
 setup(name='pipelinewise-tap-snowflake',
-      version='1.0.5',
+      version='1.1.0',
       description='Singer.io tap for extracting data from Snowflake - PipelineWise compatible',
       long_description=long_description,
       long_description_content_type='text/markdown',
