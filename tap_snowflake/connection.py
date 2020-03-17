aff6e11fc362d4ed0453808dd8ac5a24f81a3062
Peter Kosztolanyi <peter.kosztolanyi@transferwise.com>
2020-03-17 12:56:33 +0000

    Fixed pylint and unified quotes

--- tap_snowflake/connection.py
+++ tap_snowflake/connection.py
@@ -2,24 +2,30 @@
 
 import backoff
 import singer
+import sys
 import snowflake.connector
 
 LOGGER = singer.get_logger('tap_snowflake')
 
 
 def retry_pattern():
+    """Retry pattern decorator used when connecting to snowflake
+    """
     return backoff.on_exception(backoff.expo,
-                      (snowflake.connector.errors.OperationalError),
-                      max_tries=5,
-                      on_backoff=log_backoff_attempt,
-                      factor=2)
+                                snowflake.connector.errors.OperationalError,
+                                max_tries=5,
+                                on_backoff=log_backoff_attempt,
+                                factor=2)
 
 
 def log_backoff_attempt(details):
-    LOGGER.info("Error detected communicating with Snowflake, triggering backoff: %d try", details.get("tries"))
+    """Log backoff attempts used by retry_pattern
+    """
+    LOGGER.info('Error detected communicating with Snowflake, triggering backoff: %d try', details.get('tries'))
 
 
 def validate_config(config):
+    """Validate configuration dictionary"""
     errors = []
     required_config_keys = [
         'account',
@@ -32,12 +38,14 @@ def validate_config(config):
     # Check if mandatory keys exist
     for k in required_config_keys:
         if not config.get(k, None):
-            errors.append("Required key is missing from config: [{}]".format(k))
+            errors.append(f'Required key is missing from config: [{k}]')
 
     return errors
 
 
 class SnowflakeConnection:
+    """Class to manage connection to snowflake data warehouse"""
+
     def __init__(self, connection_config):
         """
         connection_config:      Snowflake connection details
@@ -47,11 +55,12 @@ class SnowflakeConnection:
         if len(config_errors) == 0:
             self.connection_config = connection_config
         else:
-            LOGGER.error("Invalid configuration:\n   * {}".format('\n   * '.join(config_errors)))
-            exit(1)
+            LOGGER.error('Invalid configuration:\n   * %s', '\n   * '.join(config_errors))
+            sys.exit(1)
 
 
     def open_connection(self):
+        """Connect to snowflake database"""
         return snowflake.connector.connect(
             user=self.connection_config['user'],
             password=self.connection_config['password'],
@@ -66,11 +75,13 @@ class SnowflakeConnection:
 
     @retry_pattern()
     def connect_with_backoff(self):
+        """Connect to snowflake database and retry automatically a few times if fails"""
         return self.open_connection()
 
 
     def query(self, query, params=None):
-        LOGGER.info("SNOWFLAKE - Running query: {}".format(query))
+        """Run a query in snowflake"""
+        LOGGER.info('SNOWFLAKE - Running query: %s', query)
         with self.connect_with_backoff() as connection:
             with connection.cursor(snowflake.connector.DictCursor) as cur:
                 cur.execute(
@@ -82,4 +93,3 @@ class SnowflakeConnection:
                     return cur.fetchall()
 
                 return []
-
