9890804afdc1f5902291b70fb9ec4fbbef5fe19d
Peter Kosztolanyi <peter.kosztolanyi@transferwise.com>
2020-03-17 10:56:47 +0000

    Bump snowflake-connector to 2.2.2

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(name='pipelinewise-tap-snowflake',
       py_modules=['tap_snowflake'],
       install_requires=[
             'pipelinewise-singer-python==1.*',
-            'snowflake-connector-python==2.0.4',
+            'snowflake-connector-python==2.2.2',
             'backoff==1.8.0',
             'pendulum==1.2.0',
             'pytz==2018.4',
