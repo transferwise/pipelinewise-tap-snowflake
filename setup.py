41dfb02d8d0f53f340dfcab2dff4a6bc774a044e
Jeet Parekh <jeet.parekh@transferwise.com>
2022-03-04 15:00:50 +0000

    release v3.0.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
     long_description = f.read()
 
 setup(name='pipelinewise-tap-snowflake',
-      version='2.0.3',
+      version='3.0.0',
       description='Singer.io tap for extracting data from Snowflake - PipelineWise compatible',
       long_description=long_description,
       long_description_content_type='text/markdown',
